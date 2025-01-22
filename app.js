969947299e71645b6a029e49b0ab4131f2a8fd9b
Kyrylo <kiri.goroshenko@gmail.com>
2025-01-22 12:02:21 +0100

    first view of product

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require("http");
+const path = require('path');
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var authorize =  require("./routes/authorize")
@@ -6,6 +7,10 @@ var authorize =  require("./routes/authorize")
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser("40a4321994a085d28587afd2621efd7be9e91922654792c763d2e8ef391a15e6"));
+
+app.use(express.static(path.join(__dirname, './public')));
+
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
@@ -14,6 +19,18 @@ app.get("/", authorize, (req, res) => {
   res.render("app", { user: req.user });
 });
 
+const product = {
+  id: 1,
+  name: "Gaming Laptop",
+  description: "High-performance laptop with NVIDIA GeForce RTX, Intel i9, and 16GB RAM.",
+  price: 1999.99,
+  image: "images/potatoes-scaled.jpg" // ścieżka do obrazu w folderze public
+};
+
+app.get("/product",(reg,res) => {
+  res.render("product", {product});
+}
+)
 app.get("/logout", authorize, (req, res) => {
   res.cookie("user", "", { maxAge: -1 });
   res.redirect("/");
